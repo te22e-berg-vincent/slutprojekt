c57362ed82a6eb768c6697ca49a2adf65dc0568e
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-03-12 10:26:01 +0100

    grid

--- SlutProjekt/Program.cs
+++ SlutProjekt/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "hej");
+Raylib.SetTargetFPS(60);
+int blockSize = 32;
+int[,] grid = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+while (!Raylib.WindowShouldClose())
+{
+
+    
+Raylib.BeginDrawing();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Raylib.EndDrawing();
+
+}
